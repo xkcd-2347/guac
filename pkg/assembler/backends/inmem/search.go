73aca47287118fb1cc9c10393c6f92d8d01177da
mrizzi <mrizzi@redhat.com>
2023-10-18 10:32:24 +0200

    Correlation: FindVulnerability implementation based on 'guacone query vuln'
    Signed-off-by: mrizzi <mrizzi@redhat.com>

--- pkg/assembler/backends/inmem/search.go
+++ pkg/assembler/backends/inmem/search.go
@@ -20,7 +20,10 @@ import (
 	"fmt"
 
 	"github.com/guacsec/guac/pkg/assembler/graphql/model"
+	"github.com/guacsec/guac/pkg/assembler/helpers"
+	"github.com/guacsec/guac/pkg/misc/depversion"
 	"github.com/vektah/gqlparser/v2/gqlerror"
+	"golang.org/x/exp/maps"
 )
 
 var edgesAllowed = []model.Edge{
@@ -99,4 +102,197 @@ func (c *demoClient) FindTopLevelPackagesRelatedToVulnerability(ctx context.Cont
 		}
 	}
 	return result, nil
+}
+
+// FindVulnerability is based upon the "guacone query vuln" CLI command (i.e. [github.com/guacsec/guac/cmd/guacone/cmd/vulnerability.go])
+func (c *demoClient) FindVulnerability(ctx context.Context, purl string) ([]model.CertifyVulnOrCertifyVEXStatement, error) {
+
+	pkgInput, err := helpers.PurlToPkg(purl)
+	if err != nil {
+		return nil, gqlerror.Errorf("failed to parse PURL: %v", err)
+	}
+
+	pkgQualifierFilter := []*model.PackageQualifierSpec{}
+	for _, qualifier := range pkgInput.Qualifiers {
+		pkgQualifierFilter = append(pkgQualifierFilter, &model.PackageQualifierSpec{
+			Key:   qualifier.Key,
+			Value: &qualifier.Value,
+		})
+	}
+
+	pkgFilter := &model.PkgSpec{
+		Type:       &pkgInput.Type,
+		Namespace:  pkgInput.Namespace,
+		Name:       &pkgInput.Name,
+		Version:    pkgInput.Version,
+		Subpath:    pkgInput.Subpath,
+		Qualifiers: pkgQualifierFilter,
+	}
+	pkgResponse, err := c.Packages(ctx, pkgFilter)
+	if err != nil {
+		return nil, gqlerror.Errorf("error querying for package: %v", err)
+	}
+	if len(pkgResponse) != 1 {
+		return nil, gqlerror.Errorf("failed to locate package based on purl")
+	}
+
+	vulnerabilities := []model.CertifyVulnOrCertifyVEXStatement{}
+
+	if pkgResponse[0].Type != helpers.PurlTypeGuac {
+		vulnPath, err := c.queryVulnsViaPackageNeighbors(ctx, pkgResponse[0].Namespaces[0].Names[0].Versions[0].ID, []model.Edge{model.EdgePackageCertifyVuln, model.EdgePackageCertifyVexStatement})
+		if err != nil {
+			return nil, gqlerror.Errorf("error querying neighbor: %v", err)
+		}
+		vulnerabilities = append(vulnerabilities, vulnPath...)
+	}
+	depVulnPath, err := c.searchDependencyPackages(ctx, pkgResponse[0].Namespaces[0].Names[0].Versions[0].ID, 10)
+	if err != nil {
+		return nil, gqlerror.Errorf("error searching dependency packages match: %v", err)
+	}
+	vulnerabilities = append(vulnerabilities, depVulnPath...)
+
+	//vexStatements, err := c.CertifyVEXStatement(ctx, &model.CertifyVEXStatementSpec{})
+	//if err != nil {
+	//	return nil, gqlerror.Errorf("FindVulnerability failed with err: %v", err)
+	//}
+	//idProduct := pkgResponse[0].Namespaces[0].Names[0].Versions[0].ID
+	//for _, vexStatement := range vexStatements {
+	//	path, err := c.Path(ctx, vexStatement.ID, idProduct, 10, edgesAllowed)
+	//	if err == nil {
+	//		vulnerabilities = append(vulnerabilities, path[0].(*model.CertifyVEXStatement))
+	//	}
+	//}
+	//vulnStatements, err := c.CertifyVuln(ctx, &model.CertifyVulnSpec{})
+	//if err != nil {
+	//	return nil, gqlerror.Errorf("FindVulnerability failed with err: %v", err)
+	//}
+	//for _, vuln := range vulnStatements {
+	//	path, err := c.Path(ctx, vuln.ID, idProduct, 10, edgesAllowed)
+	//	if err == nil {
+	//		certifyVuln := path[0].(*model.CertifyVuln)
+	//		if certifyVuln.Vulnerability.Type != noVulnType {
+	//			vulnerabilities = append(vulnerabilities, certifyVuln)
+	//		}
+	//	}
+	//}
+
+	return vulnerabilities, nil
+}
+
+func (c *demoClient) queryVulnsViaPackageNeighbors(ctx context.Context, pkgVersionID string, edgeTypes []model.Edge) ([]model.CertifyVulnOrCertifyVEXStatement, error) {
+	pkgVersionNeighborResponse, err := c.Neighbors(ctx, pkgVersionID, edgeTypes)
+	if err != nil {
+		return nil, fmt.Errorf("error querying neighbor for vulnerability: %w", err)
+	}
+	var vulnerabilities []model.CertifyVulnOrCertifyVEXStatement
+	for _, neighbor := range pkgVersionNeighborResponse {
+		if certifyVuln, ok := neighbor.(*model.CertifyVuln); ok {
+			if certifyVuln.Vulnerability.Type != noVulnType {
+				vulnerabilities = append(vulnerabilities, certifyVuln)
+			}
+		}
+		if certifyVex, ok := neighbor.(*model.CertifyVEXStatement); ok {
+			vulnerabilities = append(vulnerabilities, certifyVex)
+		}
+	}
+	return vulnerabilities, nil
+}
+
+func (c *demoClient) searchDependencyPackages(ctx context.Context, topPkgID string, maxLength int) ([]model.CertifyVulnOrCertifyVEXStatement, error) {
+	//var vulnerabilities []model.CertifyVulnOrCertifyVEXStatement
+	vulnerabilities := map[string]model.CertifyVulnOrCertifyVEXStatement{}
+	queue := make([]string, 0) // the queue of nodes in bfs
+	type dfsNode struct {
+		expanded bool // true once all node neighbors are added to queue
+		parent   string
+		depth    int
+	}
+	nodeMap := map[string]dfsNode{}
+
+	nodeMap[topPkgID] = dfsNode{}
+	queue = append(queue, topPkgID)
+
+	for len(queue) > 0 {
+		now := queue[0]
+		queue = queue[1:]
+		nowNode := nodeMap[now]
+
+		if maxLength != 0 && nowNode.depth >= maxLength {
+			break
+		}
+
+		isDependencyNeighborResponses, err := c.Neighbors(ctx, now, []model.Edge{model.EdgePackageIsDependency})
+		if err != nil {
+			return nil, fmt.Errorf("failed getting package neighbors:%w", err)
+		}
+		for _, neighbor := range isDependencyNeighborResponses {
+			if isDependency, ok := neighbor.(*model.IsDependency); ok {
+				if isDependency.DependencyPackage.Type == helpers.PurlTypeGuac {
+					continue
+				}
+
+				depPkgFilter := &model.PkgSpec{
+					Type:      &isDependency.DependencyPackage.Type,
+					Namespace: &isDependency.DependencyPackage.Namespaces[0].Namespace,
+					Name:      &isDependency.DependencyPackage.Namespaces[0].Names[0].Name,
+				}
+
+				depPkgResponse, err := c.Packages(ctx, depPkgFilter)
+				if err != nil {
+					return nil, fmt.Errorf("error querying for dependent package: %w", err)
+				}
+
+				depPkgVersionsMap := map[string]string{}
+				depPkgVersions := []string{}
+				for _, depPkgVersion := range depPkgResponse[0].Namespaces[0].Names[0].Versions {
+					depPkgVersions = append(depPkgVersions, depPkgVersion.Version)
+					depPkgVersionsMap[depPkgVersion.Version] = depPkgVersion.ID
+				}
+
+				matchingDepPkgVersions, err := depversion.WhichVersionMatches(depPkgVersions, isDependency.VersionRange)
+				if err != nil {
+					// TODO(jeffmendoza): depversion is not handling all/new possible
+					// version ranges from deps.dev. Continue here to report possible
+					// vulns even if some paths cannot be followed.
+					matchingDepPkgVersions = nil
+				}
+
+				for matchingDepPkgVersion := range matchingDepPkgVersions {
+					matchingDepPkgVersionID := depPkgVersionsMap[matchingDepPkgVersion]
+					vulns, err := c.queryVulnsViaPackageNeighbors(ctx, matchingDepPkgVersionID, []model.Edge{model.EdgePackageCertifyVuln, model.EdgePackageCertifyVexStatement})
+					if err != nil {
+						return nil, fmt.Errorf("error querying neighbor: %w", err)
+					}
+					//vulnerabilities = append(vulnerabilities, vulns...)
+					for _, vuln := range vulns {
+						if certifyVuln, ok := vuln.(*model.CertifyVuln); ok {
+							if certifyVuln.Vulnerability.Type != noVulnType {
+								vulnerabilities[certifyVuln.ID] = certifyVuln
+							}
+						} else if certifyVex, ok := vuln.(*model.CertifyVEXStatement); ok {
+							vulnerabilities[certifyVex.ID] = certifyVex
+						}
+					}
+
+					dfsN, seen := nodeMap[matchingDepPkgVersionID]
+					if !seen {
+						dfsN = dfsNode{
+							parent: now,
+							depth:  nowNode.depth + 1,
+						}
+						nodeMap[matchingDepPkgVersionID] = dfsN
+					}
+					if !dfsN.expanded {
+						queue = append(queue, matchingDepPkgVersionID)
+					}
+				}
+			}
+		}
+
+		nowNode.expanded = true
+		nodeMap[now] = nowNode
+	}
+
+	//return vulnerabilities, nil
+	return maps.Values(vulnerabilities), nil
 }
--- pkg/assembler/backends/inmem/search_test.go
+++ pkg/assembler/backends/inmem/search_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/guacsec/guac/internal/testing/testdata"
 	"github.com/guacsec/guac/pkg/assembler/backends"
 	"github.com/guacsec/guac/pkg/assembler/graphql/model"
+	"github.com/guacsec/guac/pkg/assembler/helpers"
 )
 
 var p5 = &model.PkgInputSpec{
@@ -32,6 +33,12 @@ var p5out = &model.Package{
 			}},
 		}},
 	}},
+}
+
+var p6 = &model.PkgInputSpec{
+	Type:    p2.Type,
+	Name:    p2.Name,
+	Version: ptrfrom.String("3.0.4"),
 }
 
 func Test_demoClient_FindTopLevelPackagesRelatedToVulnerability(t *testing.T) {
@@ -296,6 +303,375 @@ func Test_demoClient_FindTopLevelPackagesRelatedToVulnerability(t *testing.T) {
 			got, err := b.FindTopLevelPackagesRelatedToVulnerability(ctx, tt.query)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("FindTopLevelPackagesRelatedToVulnerability error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := cmp.Diff(tt.want, got, ignoreID); diff != "" {
+				t.Errorf("Unexpected results. (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_demoClient_FindVulnerability(t *testing.T) {
+	ctx := context.Background()
+	type IsDependency struct {
+		P1 model.PkgInputSpec
+		P2 model.PkgInputSpec
+		MF model.MatchFlags
+		ID model.IsDependencyInputSpec
+	}
+	type HasSBOM struct {
+		Sub model.PackageOrArtifactInput
+		HS  model.HasSBOMInputSpec
+	}
+	type CertifyVEXStatement struct {
+		Sub  model.PackageOrArtifactInput
+		Vuln model.VulnerabilityInputSpec
+		In   model.VexStatementInputSpec
+	}
+	type CertifyVuln struct {
+		Pkg         *model.PkgInputSpec
+		Vuln        *model.VulnerabilityInputSpec
+		CertifyVuln *model.ScanMetadataInput
+	}
+	tests := []struct {
+		name                  string
+		InPkg                 []*model.PkgInputSpec
+		InIsDependency        []IsDependency
+		InHasSBOM             []HasSBOM
+		InVulnerability       []*model.VulnerabilityInputSpec
+		InCertifyVexStatement []CertifyVEXStatement
+		InCertifyVuln         []CertifyVuln
+		query                 string
+		want                  []model.CertifyVulnOrCertifyVEXStatement
+		wantErr               bool
+	}{
+		{
+			name:  "Happy Path VEX",
+			InPkg: []*model.PkgInputSpec{p1, p2, p6},
+			// p1 is a dependency ONLY of p2
+			InIsDependency: []IsDependency{
+				{
+					P1: *p1,
+					P2: *p2,
+					MF: model.MatchFlags{Pkg: model.PkgMatchTypeSpecificVersion},
+					ID: model.IsDependencyInputSpec{
+						Justification: "test justification",
+					},
+				},
+			},
+			// both p2 and p6 are top level packages, i.e. with an SBOM
+			InHasSBOM: []HasSBOM{
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p2,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p6,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+			},
+			InVulnerability: []*model.VulnerabilityInputSpec{{
+				Type:            "cve",
+				VulnerabilityID: "CVE-2019-13110",
+			}},
+			// vulnerability relates to p1
+			InCertifyVexStatement: []CertifyVEXStatement{{
+				Sub: model.PackageOrArtifactInput{
+					Package: p1,
+				},
+				Vuln: model.VulnerabilityInputSpec{
+					Type:            "cve",
+					VulnerabilityID: "CVE-2019-13110",
+				},
+				In: model.VexStatementInputSpec{
+					VexJustification: "test justification",
+					KnownSince:       time.Unix(1e9, 0),
+				},
+			}},
+			query: helpers.PkgToPurl(p2.Type, "", p2.Name, *p2.Version, "", []string{}),
+			want: []model.CertifyVulnOrCertifyVEXStatement{
+				&model.CertifyVEXStatement{
+					Subject: p1out,
+					Vulnerability: &model.Vulnerability{
+						Type: "cve",
+						VulnerabilityIDs: []*model.VulnerabilityID{
+							{VulnerabilityID: "cve-2019-13110"},
+						},
+					},
+					VexJustification: "test justification",
+					KnownSince:       time.Unix(1e9, 0),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:  "Happy Path VEX Finds nothing",
+			InPkg: []*model.PkgInputSpec{p1, p2, p6},
+			// p1 is a dependency ONLY of p2
+			InIsDependency: []IsDependency{
+				{
+					P1: *p1,
+					P2: *p2,
+					MF: model.MatchFlags{Pkg: model.PkgMatchTypeSpecificVersion},
+					ID: model.IsDependencyInputSpec{
+						Justification: "test justification",
+					},
+				},
+			},
+			// both p2 and p6 are top level packages, i.e. with an SBOM
+			InHasSBOM: []HasSBOM{
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p2,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p6,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+			},
+			InVulnerability: []*model.VulnerabilityInputSpec{{
+				Type:            "cve",
+				VulnerabilityID: "CVE-2019-13110",
+			}},
+			// vulnerability relates to p1
+			InCertifyVexStatement: []CertifyVEXStatement{{
+				Sub: model.PackageOrArtifactInput{
+					Package: p1,
+				},
+				Vuln: model.VulnerabilityInputSpec{
+					Type:            "cve",
+					VulnerabilityID: "CVE-2019-13110",
+				},
+				In: model.VexStatementInputSpec{
+					VexJustification: "test justification",
+					KnownSince:       time.Unix(1e9, 0),
+				},
+			}},
+			query:   helpers.PkgToPurl(p6.Type, "", p6.Name, *p6.Version, "", []string{}),
+			want:    []model.CertifyVulnOrCertifyVEXStatement{},
+			wantErr: false,
+		},
+		{
+			name:  "Happy Path Vuln",
+			InPkg: []*model.PkgInputSpec{p1, p2, p6},
+			// p1 is a dependency ONLY of p2
+			InIsDependency: []IsDependency{
+				{
+					P1: *p1,
+					P2: *p2,
+					MF: model.MatchFlags{Pkg: model.PkgMatchTypeSpecificVersion},
+					ID: model.IsDependencyInputSpec{
+						Justification: "test justification",
+					},
+				},
+			},
+			// both p2 and p6 are top level packages, i.e. with an SBOM
+			InHasSBOM: []HasSBOM{
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p2,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p6,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+			},
+			InVulnerability: []*model.VulnerabilityInputSpec{{
+				Type:            "cve",
+				VulnerabilityID: "CVE-2019-13110",
+			}},
+			// vulnerability relates to p1
+			InCertifyVuln: []CertifyVuln{{
+				Pkg: p1,
+				Vuln: &model.VulnerabilityInputSpec{
+					Type:            "cve",
+					VulnerabilityID: "CVE-2019-13110",
+				},
+				CertifyVuln: &model.ScanMetadataInput{
+					Collector:      "test collector",
+					Origin:         "test origin",
+					ScannerVersion: "v1.0.0",
+					ScannerURI:     "test scanner uri",
+					DbVersion:      "2023.01.01",
+					DbURI:          "test db uri",
+					TimeScanned:    testdata.T1,
+				},
+			}},
+			query: helpers.PkgToPurl(p2.Type, "", p2.Name, *p2.Version, "", []string{}),
+			want: []model.CertifyVulnOrCertifyVEXStatement{
+				&model.CertifyVuln{
+					Package: p1out,
+					Vulnerability: &model.Vulnerability{
+						Type: "cve",
+						VulnerabilityIDs: []*model.VulnerabilityID{
+							{VulnerabilityID: "cve-2019-13110"},
+						},
+					},
+					Metadata: &model.ScanMetadata{
+						Collector:      "test collector",
+						Origin:         "test origin",
+						ScannerVersion: "v1.0.0",
+						ScannerURI:     "test scanner uri",
+						DbVersion:      "2023.01.01",
+						DbURI:          "test db uri",
+						TimeScanned:    testdata.T1,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:  "Happy Path Vuln Finds nothing",
+			InPkg: []*model.PkgInputSpec{p1, p2, p6},
+			// p1 is a dependency ONLY of p2
+			InIsDependency: []IsDependency{
+				{
+					P1: *p1,
+					P2: *p2,
+					MF: model.MatchFlags{Pkg: model.PkgMatchTypeSpecificVersion},
+					ID: model.IsDependencyInputSpec{
+						Justification: "test justification",
+					},
+				},
+			},
+			// both p2 and p6 are top level packages, i.e. with an SBOM
+			InHasSBOM: []HasSBOM{
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p2,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+				{
+					Sub: model.PackageOrArtifactInput{
+						Package: p6,
+					},
+					HS: model.HasSBOMInputSpec{
+						URI: "test uri",
+					},
+				},
+			},
+			InVulnerability: []*model.VulnerabilityInputSpec{{
+				Type:            "cve",
+				VulnerabilityID: "CVE-2019-13110",
+			}},
+			// vulnerability relates to p1
+			InCertifyVuln: []CertifyVuln{{
+				Pkg: p1,
+				Vuln: &model.VulnerabilityInputSpec{
+					Type:            "cve",
+					VulnerabilityID: "CVE-2019-13110",
+				},
+				CertifyVuln: &model.ScanMetadataInput{
+					Collector:      "test collector",
+					Origin:         "test origin",
+					ScannerVersion: "v1.0.0",
+					ScannerURI:     "test scanner uri",
+					DbVersion:      "2023.01.01",
+					DbURI:          "test db uri",
+					TimeScanned:    testdata.T1,
+				},
+			}},
+			query:   helpers.PkgToPurl(p6.Type, "", p6.Name, *p6.Version, "", []string{}),
+			want:    []model.CertifyVulnOrCertifyVEXStatement{},
+			wantErr: false,
+		}}
+	ignoreID := cmp.FilterPath(func(p cmp.Path) bool {
+		return strings.Compare(".ID", p[len(p)-1].String()) == 0
+	}, cmp.Ignore())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := backends.Get("inmem", nil, nil)
+			if err != nil {
+				t.Fatalf("Could not instantiate testing backend: %v", err)
+			}
+
+			_, err = b.IngestPackages(ctx, tt.InPkg)
+			if err != nil {
+				t.Fatalf("did not get expected ingest error, %v", err)
+			}
+			if err != nil {
+				return
+			}
+
+			for _, isDependency := range tt.InIsDependency {
+				_, err := b.IngestDependency(ctx, isDependency.P1, isDependency.P2, isDependency.MF, isDependency.ID)
+				if err != nil {
+					t.Fatalf("did not get expected ingest error, %v", err)
+				}
+				if err != nil {
+					return
+				}
+			}
+
+			for _, hasSBOM := range tt.InHasSBOM {
+				_, err := b.IngestHasSbom(ctx, hasSBOM.Sub, hasSBOM.HS)
+				if err != nil {
+					t.Fatalf("did not get expected ingest error, %v", err)
+				}
+				if err != nil {
+					return
+				}
+			}
+
+			_, err = b.IngestVulnerabilities(ctx, tt.InVulnerability)
+			if err != nil {
+				t.Fatalf("did not get expected ingest error, %v", err)
+			}
+			if err != nil {
+				return
+			}
+
+			for _, vexStatement := range tt.InCertifyVexStatement {
+				_, err := b.IngestVEXStatement(ctx, vexStatement.Sub, vexStatement.Vuln, vexStatement.In)
+				if err != nil {
+					t.Fatalf("did not get expected ingest error, %v", err)
+				}
+				if err != nil {
+					return
+				}
+			}
+
+			for _, vuln := range tt.InCertifyVuln {
+				_, err := b.IngestCertifyVuln(ctx, *vuln.Pkg, *vuln.Vuln, *vuln.CertifyVuln)
+				if err != nil {
+					t.Fatalf("did not get expected ingest error, %v", err)
+				}
+				if err != nil {
+					return
+				}
+			}
+
+			got, err := b.FindVulnerability(ctx, tt.query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindVulnerability error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if diff := cmp.Diff(tt.want, got, ignoreID); diff != "" {
